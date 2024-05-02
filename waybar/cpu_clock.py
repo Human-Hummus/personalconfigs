daed990b347dddf9c1f20570fc6268607f805e85
HumanHummus <michael.a.deross@gmail.com>
2024-05-01 19:18:52 -0700

    added clock speed waybar module

---
+++ waybar/cpu_clock.py
@@ -0,0 +1,17 @@
+#!/nix/store/6sb0k4fh4sib7avqdv939s5nlvpi1v8s-system-path/bin/python3
+
+import os, sys, time
+
+while True:
+	cpu_cores = 0
+	total_clock = 0
+	avg_clock = 0
+	for i in os.popen("cat /proc/cpuinfo | grep 'cpu MHz'").read().split("\n"):
+		if i == "":
+			continue
+		cpu_cores+=1
+		total_clock+=float(i.split(":")[1].strip())
+
+	avg_clock=total_clock/cpu_cores
+	print("<small>⏲ " + str(round(avg_clock))+"</small>")
+	time.sleep(3)
---
+++ waybar/cpu_clock.sh
@@ -0,0 +1,5 @@
+while :
+do
+	python3 /etc/nixos/configs/waybar/cpu_clock.py
+	sleep 3
+done
