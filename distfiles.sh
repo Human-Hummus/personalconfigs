21ed031198352410faabe74789b7cea58b317701
HumanHummus <michael.a.deross@gmail.com>
2024-05-06 18:37:36 -0700

    added progress indicators to distfiles.sh; added portage world file

--- distfiles.sh
+++ distfiles.sh
@@ -1,25 +1,41 @@
 doas echo "has permissions"
 
 if [ $# -eq 1 ]; then
+	echo "doing non-routine tasks"
+	echo "changing ownerhip of /home/user to user"
 	doas chown -R user /home/user
+	echo "changing ownership of /configs to user"
 	doas chown -R user /configs
-	doas chmod +x /configs/rofi_scripts/special_chars.py
+	echo "making user's default shell bash"
 	doas usermod --shell /bin/bash user
-	doas cp doas.conf /etc/doas.conf
 fi
 
+echo "distributing routine files"
+
+
+echo "distributing doas config file"
+doas cp doas.conf /etc/doas.conf
+
+
+echo "distributing hyprland config file"
 rm /home/user/.config/hypr/hyprland.conf
 mkdir -p /home/user/.config/hypr/
 cp /configs/hyprland.conf /home/user/.config/hypr/hyprland.conf 
 
-
+echo "distributing waybar config"
 rm /home/user/.config/waybar -r
 cp waybar /home/user/.config/ -r
 
-
+echo "distributing fish config"
 mkdir -p /home/user/.config/fish
 cp config.fish /home/user/.config/fish/
 
+
+echo "distributing portage files"
+echo "distributing make.conf"
 doas mkdir -p /etc/portage
 doas cp make.conf /etc/portage
 
+echo "distributing world file"
+doas mkdir -p /var/lib/portage/
+doas cp portage_world_file /var/lib/portage/world
